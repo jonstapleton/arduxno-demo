d487f9869421167e7926d9b98ff555c69ea9dc4d
Jon Stapleton <stapleton.jon.michael@gmail.com>
2021-11-06 20:50:08 -0400

    Revert "It works now! I think I'm ready to test actually getting the hardware values into the VM"
    This reverts commit 4637b131f2f5ef26b593ed7e628d5e445f185830.

--- lib/Hardware/src/Hardware.cpp
+++ lib/Hardware/src/Hardware.cpp
@@ -15,37 +15,77 @@
 int previous = 0x00;
 Hardware hardware;
 
-Bounce pins[8];
+Bounce b = Bounce();
+
+// Bounce b = Bounce();
+
+int is_event(Pin_Event *e) {
+    // check to see if there are new events
+    get_pins(&hardware);
+    if(hardware.state != previous) {
+        e->snapshot = hardware.state;
+        e->type = PINDOWN;
+        previous = hardware.state;
+        return 1;
+    }
+    return 0; // no new events
+}
+
+// int setup_hardware() {
+//     // store pin definitions in hardware struct
+//     h.pins[0] = PIN_1;
+//     h.pins[1] = PIN_2;
+//     h.pins[2] = PIN_3;
+//     h.pins[3] = PIN_4;
+//     h.pins[4] = PIN_5;
+//     h.pins[5] = PIN_6;
+//     h.pins[6] = PIN_7;
+//     h.pins[7] = PIN_8;
+
+//     // iterate over the pins and set them up as inputs
+//     for(int i=0;i<8;i++) {
+//         pinMode(h.pins[i], INPUT_PULLUP);
+//     }
+//     pinMode(13, OUTPUT);
+
+//     // set the initial bitfield based on pin state
+//     get_pins(&h);
+//     return 1;
+// }
 
 int setup_hardware() {
-    for(int i=0;i<8;i++) {
-        pins[i] = Bounce();
-        pins[i].attach(i, INPUT_PULLUP);
-    }
-    // set LED as output
+    b.attach(0, INPUT_PULLUP);
+    b.interval(25);
     pinMode(13, OUTPUT);
-
-    // set all pins to "off" initially in memory
     hardware.state = 0x00;
 }
 
-// Checks the state of the MCU pins and updates *h accordingly
-// returns a 1 if the state of the hardware is different than the last check
-// returns a 0 if the state of the hardware is unchanged since the last check
-int check_hardware(Hardware *h) {
+int get_pins(Hardware *h) {
+    unsigned char bitfield = 0x00;
+    for(int i=0; i<8; /* always 8 -- it has to iterate over a byte! */ i++) {
+        bitfield <<= 1;
+        bitfield += ~digitalRead(h->pins[i]);
+    }
+    h->state = bitfield; // push the bitfield to the hardware struct
+    return 1; // success
+}
+
+int check_hardware(Hardware *h) { // TODO: add debounce
+
+    b.update();
+
     int bitfield = 0x00;
     for(int i=0; i<8; /* always 8 -- it has to iterate over a byte! */ i++) {
-        pins[i].update();
         bitfield <<= 1;
-        bitfield += 1 - pins[i].read(); // Flip HIGH to LOW & vice-versa--pins are pulled down
+        // TODO: actually load in multiple buttons
+        bitfield += 1 - b.read(); // Flip HIGH to LOW & vice-versa
     }
 
-    // *h is from the emulator -- this will probably be replaced with a HardwareEvent struct to match the Desktop emulator
-    h->state = bitfield; // update the Hardware struct with new pin readings
+    h->state = bitfield;
     if(hardware.state != bitfield) {
-    hardware.state = bitfield; // `hardware` is persistent across cycles
-        return 1; // state has changed
+        hardware.state = bitfield;
+        return 1;
     } else {
-        return 0; // state is unchanged
+        return 0;
     }
 }
